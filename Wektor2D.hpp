4c8083088729aaaf60fa238215b8a937efbe46ec
Struskawka <indi.15.15@gmail.com>
2022-10-23 12:01:19 +0000

    podejscie 2

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -6,10 +6,10 @@ class Wektor2D
 	Wektor2D(){x=0;y=0;}
 	Wektor2D(double xx, double yy){x=xx;y=yy;}
 		
-	void setx(double xx){x=xx;}
-	void sety(double yy){y=yy;}
-    double  getx()      {return x;}
-	double  gety()      {return y;}
+	void setX(double xx){x=xx;}
+	void setY(double yy){y=yy;}
+  double  getX()      {return x;}
+	double  getY()      {return y;}
 
 	private:
 	double x, y;
@@ -18,10 +18,10 @@ class Wektor2D
 
 Wektor2D operator+(Wektor2D w1, Wektor2D w2)
 {
-	return Wektor2D( w1.getx() + w2.getx(), w1.gety() + w2.gety() );
+	return Wektor2D( w1.getX() + w2.getX(), w1.getY() + w2.getY() );
 }
 
 double operator*(Wektor2D w1, Wektor2D w2)
 {
-	return w1.getx() * w2.getx() + w1.gety() * w2.gety();
+	return w1.getX() * w2.getX() + w1.getY() * w2.getY();
 }
